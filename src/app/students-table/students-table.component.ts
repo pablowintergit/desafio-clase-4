2cff24d06d43b4ce4e4f636cf56239d24e22d8cd
pablowinter <pablo.winter@gmail.com>
2024-03-24 01:07:13 -0300

    Implementacion de conignas con estilos v1

--- src/app/students-table/students-table.component.ts
+++ src/app/students-table/students-table.component.ts
@@ -8,13 +8,23 @@ import { Component } from '@angular/core';
 export class StudentsTableComponent {
 
     students = [
-        {nombre:"Pedro",apellido:"Garcia", fechaNacimiento:new Date(1994,2,15), estado:"activo",condicion:"promocionado"},
-        {nombre:"Juan",apellido:"Rodriguez", fechaNacimiento:new Date(1990,4,16), estado:"activo",condicion:"regular"},
-        {nombre:"Ivana",apellido:"Paez", fechaNacimiento:new Date(1992,5,21), estado:"activo",condicion:"libre"},
-        {nombre:"Luis",apellido:"Machado", fechaNacimiento:new Date(1990,4,16), estado:"baja",condicion:"libre"},
-        {nombre:"Roxana",apellido:"Santucho", fechaNacimiento:new Date(1993,8,25), estado:"activo",condicion:"promocionado"},
-        {nombre:"Juan",apellido:"Zapata", fechaNacimiento:new Date(1990,5,2), estado:"baja",condicion:"regular"},
-        {nombre:"Sabrina",apellido:"Lopez", fechaNacimiento:new Date(1992,8,26), estado:"activo",condicion:"regular"},
+        {nombre:"Pedro",apellido:"Garcia", fechaNacimiento:new Date(1994,2,15), estado:"activo",nota:9},
+        {nombre:"Juan",apellido:"Rodriguez", fechaNacimiento:new Date(1990,4,16), estado:"activo",nota:6},
+        {nombre:"Ivana",apellido:"Paez", fechaNacimiento:new Date(1992,5,21), estado:"activo",nota:3},
+        {nombre:"Luis",apellido:"Machado", fechaNacimiento:new Date(1990,4,16), estado:"baja",nota:2},
+        {nombre:"Roxana",apellido:"Santucho", fechaNacimiento:new Date(1993,8,25), estado:"activo",nota:8},
+        {nombre:"Juan",apellido:"Zapata", fechaNacimiento:new Date(1990,5,2), estado:"baja",nota:5},
+        {nombre:"Sabrina",apellido:"Lopez", fechaNacimiento:new Date(1992,8,26), estado:"activo",nota:5},
     ];
+
+    getCondicion(nota:number):string{
+        if (nota>=7){
+          return "Promocionado";
+        }else if (nota<7 && nota>4){
+          return "Regular";
+        }else{
+          return "Libre";
+        }
+    }
 }
 
